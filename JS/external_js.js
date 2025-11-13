23a345b175ee8975709c9efc2a7d083b7dbfd460
kanwadeashish <ashish.xworkz@gmail.com>
2025-11-13 22:53:01 +0530

    Example of using a external js file.

---
+++ JS/external_js.js
@@ -0,0 +1,6 @@
+console.log("Example of external javascript.");
+console.log("Name - Ajay");
+console.log("Education - B.E");
+console.log("Age - 24");
+console.log("Occupation - Web developer");
+console.log("Salary - 9LPAA");
