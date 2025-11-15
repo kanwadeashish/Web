76b9fd791a3c4401f88bf91f2783404746ca53e2
kanwadeashish <ashish.xworkz@gmail.com>
2025-11-15 15:09:34 +0530

    Various datatypes in js.

---
+++ JS/datatype.js
@@ -0,0 +1,41 @@
+console.log("Datatypes in javascript.");
+
+var a=10;
+console.log(a);
+console.log(typeof a);
+
+var b="b";
+console.log(b);
+console.log(typeof b);
+
+var c=true;
+console.log(c);
+console.log(typeof c);
+
+var d=10000n;
+console.log(d);
+console.log(typeof d);
+
+var e=1.1;
+console.log(e);
+console.log(typeof e);
+
+var f='e';
+console.log(f);
+console.log(typeof f);
+
+var g = [1,2,3,true,"string"];
+console.log(g);
+console.log(typeof g);
+
+var h = {name:"Ajay",age:24};
+console.log(h);
+console.log(typeof h);
+
+var i;
+console.log(i);
+console.log(typeof i);
+
+var j=null;
+console.log(j);
+console.log(typeof j);
